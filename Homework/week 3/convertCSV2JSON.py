c118c9014e6a02fab9f76fc4691b2fbd345139a8
LinseyUvA <linseyschaap@tele2.nl>
2018-04-23 13:21:16 +0200

    poging 14

--- Homework/week 3/convertCSV2JSON.py
+++ Homework/week 3/convertCSV2JSON.py
@@ -14,5 +14,5 @@ namen = ("Provincies", "Gasten *1000")
 bestand = csv.DictReader(csvbestand, namen)
 
 for regel in bestand:
-    json.dump(regel, jsonbestand)
+    json.dump(regel, jsonbestand, indent = 4)
     jsonbestand.write("\n")
