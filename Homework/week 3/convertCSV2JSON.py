de53da0f63d349e9dd094b0df417f0c4fc510448
LinseyUvA <linseyschaap@tele2.nl>
2018-04-23 12:25:52 +0200

    poging 16

--- Homework/week 3/convertCSV2JSON.py
+++ Homework/week 3/convertCSV2JSON.py
@@ -7,16 +7,10 @@ This script convert a csv file into a JSON format.
 import csv
 import json
 
-csvbestand = open("data.csv", "r")
-jsonbestand = open("data.json", "w")
+with open("data.csv", "r") as f:
+    namen = ("Provincies", "Gasten *1000")
+    bestand = csv.DictReader(f, namen)
+    regels = list(reader)
 
-# data = {}
-# for lijn in csvbestand:
-#     data[lijn.split(",")[0]] = (lijn.split(",")[1])
-
-namen = ("Provincies", "Gasten *1000")
-bestand = csv.DictReader(csvbestand, namen)
-
-for regel in bestand:
-    json.dump(regel, jsonbestand)
-    jsonbestand.write("\n")
+with open("jsonbestand", "w") as f:
+    json.dump(regels, f, indent = 4)
