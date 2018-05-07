8d5e317403411023e08aef19636217602cc6275e
LinseyUvA <linseyschaap@tele2.nl>
2018-05-07 13:21:28 +0200

    test json bestand

---
+++ Homework/week 5/convertCSV2JSONOvernachtingen.py
@@ -0,0 +1,20 @@
+# Name: Linsey Schaap
+# Student number: 11036109
+"""
+This script convert a csv file into a JSON format.
+"""
+
+import csv
+import json
+
+
+csvbestand = open("overnachtingen.csv", "r")
+jsonbestand = open("overnachtingen.json", "w")
+
+namen = ("Land", "jaar", "overnachtingen * 1000")
+bestand = csv.DictReader(csvbestand, namen)
+
+# Parse the CSV into JSON
+out = json.dumps( [ regel for regel in bestand ] )
+# Save the JSON
+jsonbestand.write('{"data": ' + out + '}')
