07e1e584f56c5504767115f164ecd0ed0aeed58a
LinseyUvA <linseyschaap@tele2.nl>
2018-05-07 13:54:37 +0200

    aanpassingen

--- Homework/week 5/linkedviews.js
+++ Homework/week 5/linkedviews.js
@@ -1,19 +1,13 @@
 window.onload = function() {
-  var BLI2017 = "https://stats.oecd.org/SDMX-JSON/data/BLI/AUS+AUT+BEL+CAN+CHL+CZE+DNK+EST+FIN+FRA+DEU+GRC+HUN+ISL+IRL+ISR+ITA+JPN+KOR+LUX+MEX+NLD+NZL+NOR+POL+PRT+SVK+SVN+ESP+SWE+CHE+TUR+GBR+USA.EQ_AIRP+EQ_WATER+HS_SFRH+SW_LIFS.L.TOT/all?"
-  var BLI2014 = "https://stats.oecd.org/SDMX-JSON/data/BLI2014/AUS+AUT+BEL+CAN+CHL+CZE+DNK+EST+FIN+FRA+DEU+GRC+HUN+ISL+IRL+ISR+ITA+JPN+KOR+LUX+MEX+NLD+NZL+NOR+POL+PRT+SVK+SVN+ESP+SWE+CHE+TUR+GBR+USA.EQ_AIRP+EQ_WATER+HS_SFRH+SW_LIFS.L.TOT/all?"
 
   d3.queue()
-  .defer(d3.request, BLI2017)
-  .defer(d3.request, BLI2014)
-  .awaitAll(doFunction);
-
-  d3.select("head").append("title").text("Scatter plot")
-
+  .defer(d3.json, "overnachtingen.json")
+  .defer(d3.json, "sterren")
+  .awaitAll(grafieken);
 };
 
-
-
-function doFunction(error, response) {
+function grafieken(error, overnachtingen, sterren) {
   if (error) throw error;
+
   var projection = d3.geo.conicConformalEurope();
 }
