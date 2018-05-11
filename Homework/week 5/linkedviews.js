247f3225b9b31efd7a44e08e3ada80c24a5a1d71
LinseyUvA <linseyschaap@tele2.nl>
2018-05-11 19:34:59 +0200

    on click toegevoegd

--- Homework/week 5/linkedviews.js
+++ Homework/week 5/linkedviews.js
@@ -29,7 +29,10 @@ window.onload = function() {
           d3.select(this).style("fill", "Navy");})
       .on("mousemove", function(d) {
           infoKnop.html("Vanuit " + d.Land + " overnachten " + d.Overnachtingen + " duizend mensen in Nederland (2012)<br/>")
-        });
+        })
+      .on("click", function(d) {
+          updateBarchart(eval(d.x), d.Land)
+      })
 
     // stel hoogte en breedte vast
     var hoogte = 600
@@ -47,8 +50,15 @@ window.onload = function() {
         .attr("height", hoogte)
         .attr("width", breedte)
 
+    ster = []
+    for (var i = 0; i < sterren.length; i++) {
+      if (sterren[i].Land == "Nederland"){
+        ster.push(sterren[i])
+      }
+    }
+
     // bepaal maximale waarden en de breedte van een staaf
-    var maxWaarde = Math.max.apply(Math, sterren.map(function(d) {
+    var maxWaarde = Math.max.apply(Math, ster.map(function(d) {
       return d.aantalOvernachtingen
     }))
 
@@ -60,14 +70,15 @@ window.onload = function() {
     var y = d3.scaleLinear([maxWaarde, 0])
               .range([grafiekHoogte, 0])
 
-    x.domain(sterren.map(function(d) {
+    x.domain(ster.map(function(d) {
       return d.soortHotel
     }))
     y.domain([0, maxWaarde])
 
+
     // creëer alle staven
     var staaf = svg.selectAll("rect")
-        .data(sterren)
+        .data(ster)
         .enter()
         .append("rect")
 
@@ -97,8 +108,8 @@ window.onload = function() {
             var xPos = d3.mouse(this)[0] - marge.rechts;
             var yPos = d3.mouse(this)[1] - marge.beneden;
             infoKnop2.attr("transform", "translate(" + xPos + "," + yPos + ")")
-            infoKnop2.select("text").text(d.aantalOvernachtingen);});
-        
+            infoKnop2.select("text").text(d.aantalOvernachtingen);})
+
 
     // creëer een infoKnop
     var infoKnop2 = svg.append("g")
@@ -143,7 +154,7 @@ window.onload = function() {
     // geef de y-as een titel
     svg.append("text")
        .attr("transform", "rotate(-90)")
-       .attr("y", 0 + marge.beneden)
+       .attr("y", 0 + marge.rechts)
        .attr("x", 0 - (hoogte / 2))
        .attr("dy", "1em")
        .style("text-anchor", "middle")
@@ -158,12 +169,127 @@ window.onload = function() {
       .attr("text-anchor", "middle")
       .text("Het aantal overnachtingen dat plaats vindt in verschillende soorten hotels");
 
+
+    function updateBarchart(error, land) {
+      d3.selectAll("rect").remove()
+      d3.selectAll("text").remove()
+      ster = []
+      for (var i = 0; i < sterren.length; i++) {
+        if (sterren[i].Land == land){
+          ster.push(sterren[i])
+          console.log(ster)
+        }
+      }
+      // bepaal maximale waarden en de breedte van een staaf
+      var maxWaarde = Math.max.apply(Math, ster.map(function(d) {
+        return d.aantalOvernachtingen
+      }))
+
+      // maak een schaalfunctie voor de x waarden
+      var x = d3.scaleBand()
+                .range([0, grafiekBreedte], .1)
+
+      // maak een schaalfunctie voor de y waarden
+      var y = d3.scaleLinear([maxWaarde, 0])
+                .range([grafiekHoogte, 0])
+
+      x.domain(ster.map(function(d) {
+        return d.soortHotel
+      }))
+      y.domain([0, maxWaarde])
+
+      // creëer alle staven
+      var staaf = svg.selectAll("rect")
+          .data(ster)
+          .enter()
+          .append("rect")
+
+          // zet de staaf op de juiste positie
+          .attr("x", function(d, i) {
+              return x(d.soortHotel) + marge.links + grens; })
+          .attr("y", function(d) {
+              return y(d.aantalOvernachtingen) + marge.boven; })
+
+          // geef de staaf de juiste hoogte en breedte mee
+          .attr("width", x.bandwidth() - grens)
+          .attr("height", function(d) {
+              return grafiekHoogte - y(d.aantalOvernachtingen); })
+
+          // geef de staaf een kleur en rand
+          .attr("fill", "Navy")
+          .attr("stroke", "Black")
+
+          // maak de staven interactief
+          .on("mouseover", function() {
+              infoKnop2.style("display", null);
+              d3.select(this).style("fill", "SlateGray");})
+          .on("mouseout", function() {
+              infoKnop2.style("display", "none");
+              d3.select(this).style("fill", "Navy");})
+          .on("mousemove", function(d) {
+              var xPos = d3.mouse(this)[0] - marge.rechts;
+              var yPos = d3.mouse(this)[1] - marge.beneden;
+              infoKnop2.attr("transform", "translate(" + xPos + "," + yPos + ")")
+              infoKnop2.select("text").text(d.aantalOvernachtingen);})
+
+
+      // creëer een infoKnop
+      var infoKnop2 = svg.append("g")
+                         .attr("class", "tooltip")
+                         .style("display", "none");
+
+      // voeg de informatie toe aan de infoKnop
+      infoKnop2.append("text")
+               .attr("x", 15)
+               .attr("dy", "1.2em")
+               .style("text-anchor", "middle")
+               .attr("font-size", "12px")
+               .attr("font-weight", "bold");
+
+      // creëer een x-as
+      var asX = d3.axisBottom(x);
+
+      // voeg de x-as en waarden toe
+      svg.append("g")
+         .attr("class", "axis")
+         .attr("transform", "translate(" + marge.links + "," + (grafiekHoogte + marge.boven) + ")")
+         .call(asX)
+         .attr("font-size", "10px");
+
+      // geef de x-as een titel
+      svg.append("text")
+         .attr("x", (breedte - marge.rechts + marge.links)/ 2 )
+         .attr("y",  y(0) + marge.beneden + marge.boven)
+         .style("text-anchor", "middle")
+         .text("Soort Hotel");
+
+      // creëer een y-as
+      var asY = d3.axisLeft(y)
+
+      // voeg de y-as en waarden toe
+      svg.append("g")
+          .attr("class", "axis")
+          .attr("transform", "translate(" + marge.links + "," +  marge.boven + ")")
+          .call(asY)
+          .attr("font-size", "10px");
+
+      // geef de y-as een titel
+      svg.append("text")
+         .attr("transform", "rotate(-90)")
+         .attr("y", 0 + marge.rechts)
+         .attr("x", 0 - (hoogte / 2))
+         .attr("dy", "1em")
+         .style("text-anchor", "middle")
+         .text("Aantal overnachtingen in Nederland x1000");
+
+     // voeg een titel aan de scatterplot toe
+     svg.append("text")
+        .attr("class", "title")
+        .attr("x", breedte / 2 )
+        .attr("y", marge.boven / 2 - 20)
+        .attr("font-size", "22px")
+        .attr("text-anchor", "middle")
+        .text("Het aantal overnachtingen dat plaats vindt in verschillende soorten hotels");
+    }
   }
 };
-
-
-function barchart(error, response) {
-
-
-
-}
